c3428f57620175c54d4e1cba33d051589306dc61
Zobayer Hasan <zobayer1@gmail.com>
2012-05-28 00:58:10 +0600

    memory reduction for MKTHNUM

--- MKTHNUM.cpp
+++ MKTHNUM.cpp
@@ -16,59 +16,51 @@ const int DEPTH = 18;
 typedef struct { int lt,rt; } Node;
 
 Node tree[1 << 18];
-int data[NMAX], seg[DEPTH][NMAX], leftSide[DEPTH][NMAX];
+int a[NMAX], seg[DEPTH][NMAX], leftSide[DEPTH][NMAX];
 
-void buildtree(int root,int lt,int rt,int d) {
-    tree[root].lt=lt,tree[root].rt=rt;
-    if(lt==rt) return;
-    int mid=(lt+rt)>>1;
-    int lsame=mid-lt+1;
-    for(int i=lt;i<=rt;i++) {
-        if(seg[d][i]<data[mid]) lsame--;
-    }
-    int tl=lt,tr=mid+1,same=0;
-    for(int i=lt;i<=rt;i++) {
-        if(i==lt) leftSide[d][i]=0;
-        else leftSide[d][i]=leftSide[d][i-1];
-        if(seg[d][i]<data[mid]) leftSide[d][i]++,seg[d+1][tl++]=seg[d][i];
-        else if(seg[d][i]>data[mid]) seg[d+1][tr++]=seg[d][i];
-        else {
-            if(same<lsame) same++,leftSide[d][i]++,seg[d+1][tl++]=seg[d][i];
-            else seg[d+1][tr++]=seg[d][i];
-        }
-    }
-    buildtree(root<<1,lt,mid,d+1);
-    buildtree((root<<1)+1,mid+1,rt,d+1);
+void buildtree(int node, int lt, int rt, int d) {
+	tree[node].lt = lt,tree[node].rt = rt;
+	if(lt == rt) return;
+	int i, mid = (lt + rt) >> 1, lsame = mid - lt + 1, tl = lt, tr = mid + 1, same = 0;
+	for(i = lt; i <= rt; i++) if(seg[d][i] < a[mid]) lsame--;
+	for(i = lt; i <= rt; i++) {
+		if(i == lt) leftSide[d][i] = 0;
+		else leftSide[d][i] = leftSide[d][i - 1];
+		if(seg[d][i] < a[mid]) leftSide[d][i]++, seg[d+1][tl++] = seg[d][i];
+		else if(seg[d][i] > a[mid]) seg[d+1][tr++] = seg[d][i];
+		else {
+			if(same < lsame) same++, leftSide[d][i]++, seg[d+1][tl++] = seg[d][i];
+			else seg[d+1][tr++] = seg[d][i];
+		}
+	}
+	buildtree(node << 1, lt, mid, d + 1);
+	buildtree((node << 1) + 1, mid + 1, rt, d + 1);
 }
 
-int Query(int root,int lt,int rt,int d,int cnt) {
-    if(lt==rt) return seg[d][lt];
-    int s;
-    int ss;
-    if(lt==tree[root].lt) s=leftSide[d][rt],ss=0;
-    else s=leftSide[d][rt]-leftSide[d][lt-1],ss=leftSide[d][lt-1];
-    if(s>=cnt) return Query(root<<1,tree[root].lt+ss,tree[root].lt+ss+s-1,d+1,cnt);
-    else {
-        int mid=(tree[root].lt+tree[root].rt)>>1;
-        int bb=lt-tree[root].lt-ss;
-        int b=rt-lt+1-s;
-        return Query((root<<1)+1,mid+bb+1,mid+bb+b,d+1,cnt-s);
-    }
+int Query(int node, int lt, int rt, int d, int cnt) {
+	if(lt == rt) return seg[d][lt];
+	int s, ss;
+	if(lt == tree[node].lt) s = leftSide[d][rt], ss = 0;
+	else s = leftSide[d][rt] - leftSide[d][lt - 1], ss = leftSide[d][lt - 1];
+	if(s >= cnt) return Query(node << 1, tree[node].lt + ss, tree[node].lt + ss + s - 1, d + 1, cnt);
+	else {
+		int mid = (tree[node].lt + tree[node].rt) >> 1, bb = lt - tree[node].lt - ss, b = rt - lt + 1 - s;
+		return Query((node << 1)+1, mid + bb + 1, mid + bb + b, d + 1, cnt - s);
+	}
 }
 
 int main() {
-    int n, m;
-	scanf("%d%d",&n,&m);
-    for(int i=1;i<=n;i++) {
-        scanf("%d",&data[i]);
-        seg[1][i]=data[i];
-    }
-    sort(data+1,data+1+n);
-    buildtree(1,1,n,1);
-    while(m--) {
-        int lt,rt,k;scanf("%d%d%d",&lt,&rt,&k);
-        int res=Query(1,lt,rt,1,k);
-        printf("%d\n",res);
+	int i, n, m, lt, rt, k;
+	scanf("%d %d", &n, &m);
+	for(i = 1; i <= n; i++) {
+		scanf("%d", &a[i]);
+		seg[1][i] = a[i];
+	}
+	sort(a + 1, a + 1 + n);
+	buildtree(1, 1, n, 1);
+	while(m--) {
+        scanf("%d %d %d", &lt, &rt, &k);
+        printf("%d\n", Query(1, lt, rt, 1, k));
     }
     return 0;
 }
