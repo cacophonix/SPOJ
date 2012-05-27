404f2e41f6d3308d544d8f92ea423912e4bd6e7e
Zobayer Hasan <zobayer1@gmail.com>
2012-05-28 00:59:21 +0600

    memory reduction for MKTHNUM

--- MKTHNUM.cpp
+++ MKTHNUM.cpp
@@ -16,7 +16,7 @@ const int DEPTH = 18;
 typedef struct { int lt,rt; } Node;
 
 Node tree[1 << 18];
-int a[NMAX], seg[DEPTH][NMAX], leftSide[DEPTH][NMAX];
+int a[NMAX], seg[DEPTH][NMAX], lesser[DEPTH][NMAX];
 
 void buildtree(int node, int lt, int rt, int d) {
 	tree[node].lt = lt,tree[node].rt = rt;
@@ -24,12 +24,12 @@ void buildtree(int node, int lt, int rt, int d) {
 	int i, mid = (lt + rt) >> 1, lsame = mid - lt + 1, tl = lt, tr = mid + 1, same = 0;
 	for(i = lt; i <= rt; i++) if(seg[d][i] < a[mid]) lsame--;
 	for(i = lt; i <= rt; i++) {
-		if(i == lt) leftSide[d][i] = 0;
-		else leftSide[d][i] = leftSide[d][i - 1];
-		if(seg[d][i] < a[mid]) leftSide[d][i]++, seg[d+1][tl++] = seg[d][i];
+		if(i == lt) lesser[d][i] = 0;
+		else lesser[d][i] = lesser[d][i - 1];
+		if(seg[d][i] < a[mid]) lesser[d][i]++, seg[d+1][tl++] = seg[d][i];
 		else if(seg[d][i] > a[mid]) seg[d+1][tr++] = seg[d][i];
 		else {
-			if(same < lsame) same++, leftSide[d][i]++, seg[d+1][tl++] = seg[d][i];
+			if(same < lsame) same++, lesser[d][i]++, seg[d+1][tl++] = seg[d][i];
 			else seg[d+1][tr++] = seg[d][i];
 		}
 	}
@@ -40,8 +40,8 @@ void buildtree(int node, int lt, int rt, int d) {
 int Query(int node, int lt, int rt, int d, int cnt) {
 	if(lt == rt) return seg[d][lt];
 	int s, ss;
-	if(lt == tree[node].lt) s = leftSide[d][rt], ss = 0;
-	else s = leftSide[d][rt] - leftSide[d][lt - 1], ss = leftSide[d][lt - 1];
+	if(lt == tree[node].lt) s = lesser[d][rt], ss = 0;
+	else s = lesser[d][rt] - lesser[d][lt - 1], ss = lesser[d][lt - 1];
 	if(s >= cnt) return Query(node << 1, tree[node].lt + ss, tree[node].lt + ss + s - 1, d + 1, cnt);
 	else {
 		int mid = (tree[node].lt + tree[node].rt) >> 1, bb = lt - tree[node].lt - ss, b = rt - lt + 1 - s;
